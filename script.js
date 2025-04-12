a8eff725b7c8efc717d8ee1ea70073411971aa1e
mahoo <146084234+okriashvili@users.noreply.github.com>
2025-04-12 04:38:35 +0400

    we did some practical trainings about the dates in javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+var d = new Date()
+document.write(d.getFullYear(), '-', d.getMonth(), '-',  d.getDate(), '-', d.getMinutes(), '-', d.getSeconds(), '-', d.getDay())
+
+
+var bd = new Date
+var day = [1, 2, '...', 11]
+var month = ["january", "february", "march", "june"]
+var year = [2000, 2001, 2002, 2003]
+document.write("<br>" + year[3], '-', month[3], '-', day[3])
+
+var clock = new Date
+document.write("<br>" + "<br>" + clock.getHours(), '-', clock.getMinutes(), '-', clock.getSeconds())
+
+
+
